134af21588134df13543c5058a1848c71d924a13
Z-67 <zayn2830@gmail.com>
2024-03-02 16:32:39 +0000

    convert function added

--- frontend.py
+++ frontend.py
@@ -1,6 +1,11 @@
 import wx
 from backend import meters_feet
 
+#function for when convert button is pressed 
+def press_button(event):
+    meters = float(input_box.GetValue()) # Get the value from the input text box
+    result.SetLabel(str(meters_feet(meters))) #Call the meters_feet function from the backend program and set it to result label
+
 # Create a wxPython application object
 app = wx.App(False)
 
@@ -22,7 +27,10 @@ result = wx.StaticText(panel, label="")
 # Create a button for conversion
 button = wx.Button(panel, label="Convert")
 
-# Adds grid positions
+# Bind the press_button function to the button's click event
+button.Bind(wx.EVT_BUTTON, press_button)
+
+# Adds grid positions by putting in row and column
 sizer.Add(input_label, (0, 0))
 sizer.Add(input_box, (0, 1))
 sizer.Add(result_label, (1, 0))
